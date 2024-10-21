ac8018c9380b237a4e23fe20aafdcfae07bd3454
nanqiu <hanwen.zhang@vanderbilt.edu>
2024-10-21 16:39:23 -0500

    signin status add

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from "../../context/AuthContext";
 import styles from './Header.module.css';
 import { Button } from '@mui/material'; 
 import HandyVandyLogo from './HandyVandyLogoNoBkgd.png';
 
 const Header = () => {
   const navigate = useNavigate(); 
+  const { user, logout } = useAuth();
 
   return (
     <header className={styles.header}>
       <Button className={styles.logoButton} onClick={() => navigate('/')}>
         <img src={HandyVandyLogo} alt="HandyVandy Logo" className={styles.logoImage} />
       </Button>
-      <nav className={styles.nav}>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/signup')}>Sign Up</Button>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/login')}>Login</Button>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/create-profile')}>Create Profile</Button>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/profile-list')}>Profile List</Button>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/create-posting')}>Create Posting</Button>
-        <Button variant="contained" color="inherit" onClick={() => navigate('/posting-list')}>View Postings</Button>
-        {/* <Button variant="contained" color="inherit" onClick={() => navigate('/search-posting')}>Search Posting</Button> */}
-      </nav>
+      {user ? (
+        <nav className={styles.nav}>
+          <Button variant="contained" color="inherit" onClick={() => navigate('/profile-list')}>Profile List</Button>
+          <Button variant="contained" color="inherit" onClick={() => navigate('/create-posting')}>Create Posting</Button>
+          <Button variant="contained" color="inherit" onClick={() => navigate('/posting-list')}>View Postings</Button>
+          <Button variant="contained" color="inherit" onClick={() => { logout(); navigate('/'); }}>Sign Out</Button>
+          {/* <Button variant="contained" color="inherit" onClick={() => navigate('/search-posting')}>Search Posting</Button> */}
+        </nav>
+      ) : (
+        <nav className={styles.nav}>
+          <Button variant="contained" color="inherit" onClick={() => navigate('/signup')}>Sign Up</Button>
+          <Button variant="contained" color="inherit" onClick={() => navigate('/login')}>Login</Button>
+        </nav>
+      )}
     </header>
   );
 };
