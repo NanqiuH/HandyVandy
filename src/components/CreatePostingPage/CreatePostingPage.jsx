e209c52f440651eb18a52ee92262ddb9c9bf13cb
baileyfelsheim <113561857+baileyfelsheim@users.noreply.github.com>
2024-10-21 17:51:14 -0500

    fix comment issue

--- src/components/CreatePostingPage/CreatePostingPage.jsx
+++ src/components/CreatePostingPage/CreatePostingPage.jsx
@@ -256,7 +256,25 @@ function CreatePostingPage() {
                       <option value="physical labor">Physical Labor</option>
                       <option value="tutoring">Tutoring</option>
                       <option value="food">Food</option>
-                      {/* Add more categories here */}
+                      <option value="performance">Performance</option>
+                      <option value="travel">Travel</option>
+                      <option value="technology">Technology</option>
+                      <option value="cleaning">Cleaning</option>
+                      <option value="transportation">Transportation</option>
+                      <option value="healthcare">Healthcare</option>
+                      <option value="childcare">Childcare</option>
+                      <option value="home improvement">Home Improvement</option>
+                      <option value="pet care">Pet Care</option>
+                      <option value="event planning">Event Planning</option>
+                      <option value="education">Education</option>
+                      <option value="art and design">Art & Design</option>
+                      <option value="fitness">Fitness</option>
+                      <option value="landscaping">Landscaping</option>
+                      <option value="writing">Writing</option>
+                      <option value="music">Music</option>
+                      <option value="photography">Photography</option>
+                      <option value="automotive">Automotive</option>
+                      <option value="other">Other</option>
                     </select>
                     {categoryError && (
                       <span className={styles.errorMessage}>
