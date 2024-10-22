2e63db87280c910108427ad8df0c25ecd281d764
samuelstaylor03 <samuel.s.taylor@vanderbilt.edu>
2024-10-22 09:34:45 -0500

    category options added

--- src/components/ViewPostingsPage/ViewPostingsPage.jsx
+++ src/components/ViewPostingsPage/ViewPostingsPage.jsx
@@ -120,7 +120,28 @@ function ViewPostingsPage() {
                   onChange={handleFilterChange}
                 >
                   <option value="none">All</option>
-                  {/* Add more category options dynamically if needed */}
+                  <option value="physical labor">Physical Labor</option>
+                  <option value="tutoring">Tutoring</option>
+                  <option value="food">Food</option>
+                  <option value="performance">Performance</option>
+                  <option value="travel">Travel</option>
+                  <option value="technology">Technology</option>
+                  <option value="cleaning">Cleaning</option>
+                  <option value="transportation">Transportation</option>
+                  <option value="healthcare">Healthcare</option>
+                  <option value="childcare">Childcare</option>
+                  <option value="home improvement">Home Improvement</option>
+                  <option value="pet care">Pet Care</option>
+                  <option value="event planning">Event Planning</option>
+                  <option value="education">Education</option>
+                  <option value="art and design">Art & Design</option>
+                  <option value="fitness">Fitness</option>
+                  <option value="landscaping">Landscaping</option>
+                  <option value="writing">Writing</option>
+                  <option value="music">Music</option>
+                  <option value="photography">Photography</option>
+                  <option value="automotive">Automotive</option>
+                  <option value="other">Other</option>
                 </select>
               </label>
               {/* Sort By filter dropdown */}
