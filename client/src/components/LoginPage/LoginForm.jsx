fce6f4b237dece2d4386fc11f2b3b50cc783abf5
nanqiu <hanwen.zhang@vanderbilt.edu>
2024-12-01 19:54:12 -0600

    update home

--- client/src/components/LoginPage/LoginForm.jsx
+++ client/src/components/LoginPage/LoginForm.jsx
@@ -1,49 +1,49 @@
 import React from "react";
 import styles from "./LoginForm.module.css";
 import { auth } from "../../firebase";
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import MailIcon from '@mui/icons-material/Mail';
-import KeyIcon from '@mui/icons-material/Key';
-import IconButton from '@mui/material/IconButton';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { sendPasswordResetEmail } from 'firebase/auth';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import MailIcon from "@mui/icons-material/Mail";
+import KeyIcon from "@mui/icons-material/Key";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 function LoginForm() {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const [showPassword, setShowPassword] = React.useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          await signInWithEmailAndPassword(auth, email, password);
-          // console.log('Login successfully');
-          navigate('/posting-list');
-      } catch (error) {
-          // console.error(error);
-          alert('Login failed. Please check your email and password.');
-      }
-  }
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // console.log('Login successfully');
+      navigate("/posting-list");
+    } catch (error) {
+      // console.error(error);
+      alert("Login failed. Please check your email and password.");
+    }
+  };
 
   const handlePasswordReset = async () => {
     if (!email) {
-      alert('Please enter your email address.');
+      alert("Please enter your email address.");
       return;
     }
     try {
       await sendPasswordResetEmail(auth, email);
-      alert('Password reset email sent! Check your inbox.');
+      alert("Password reset email sent! Check your inbox.");
     } catch (error) {
-      alert('Error sending password reset email. Please try again.');
+      alert("Error sending password reset email. Please try again.");
     }
   };
 
   return (
     <form className={styles.loginForm}>
-            <div className={styles.inputGroup}>
+      <div className={styles.inputGroup}>
         <MailIcon />
         <input
           className={styles.inputField}
@@ -62,7 +62,9 @@ function LoginForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <IconButton data-testid="toggle-visibility"
+        <IconButton
+          className={styles.iconButton}
+          data-testid="toggle-visibility"
           onClick={() => setShowPassword(!showPassword)}
           edge="end"
         >
@@ -74,15 +76,19 @@ function LoginForm() {
           <input type="checkbox" className={styles.checkbox} />
           <span>Remember me</span>
         </label>
-        <span 
-         className={styles.forgotPassword} 
-         onClick={handlePasswordReset}
-         role="button"
+        <span
+          className={styles.forgotPassword}
+          onClick={handlePasswordReset}
+          role="button"
         >
-         Forgot Password?
+          Forgot Password?
         </span>
       </div>
-      <button type="submit" className={styles.loginButton} onClick={handleSubmit}>
+      <button
+        type="submit"
+        className={styles.loginButton}
+        onClick={handleSubmit}
+      >
         Login
       </button>
       <p className={styles.registerPrompt}>
